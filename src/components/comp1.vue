eb9b3c6c448815e45785c00e1bf6bbed676858a9
veeragopinath m <veeragopinath.m@gmail.com>
2022-05-06 17:28:44 +0530

    friday last

---
+++ src/components/comp1.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <input  v-model="task"  >
+        <button v-on:click="addtask(task)" v-show="isadd">add task</button>
+        <button  v-show="isedit" @click="touch(indexToUpdate)" >update</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:comp1
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/comp2.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+       <div v-for="(item,id) in currenttask"  class="row">
+       <li style="list-style: none">
+        <input type="checkbox" @click="strike(item,id)" >{{item}}</li>
+       <button @click="changeitem(item,id)">edit</button>
+       <button @click="deleteitem(item,id)">delete</button>
+       </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:comp2
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
